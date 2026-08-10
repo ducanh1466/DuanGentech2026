ea4d744eb5b076c8cd4c058038d099bc49022eae
ducanhne12 <150917348+ducanhne12@users.noreply.github.com>
2026-08-10 11:09:23 +0700

    fix loi xoa danh muc

--- views/layouts/admin_layout.php
+++ views/layouts/admin_layout.php
@@ -172,6 +172,22 @@
 
             <!-- Content -->
             <div class="admin-content">
+                <?php if (isset($_SESSION['success'])): ?>
+                    <div class="alert alert-success alert-dismissible fade show" role="alert">
+                        <?= $_SESSION['success'] ?>
+                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                    </div>
+                    <?php unset($_SESSION['success']); ?>
+                <?php endif; ?>
+
+                <?php if (isset($_SESSION['error'])): ?>
+                    <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                        <?= $_SESSION['error'] ?>
+                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                    </div>
+                    <?php unset($_SESSION['error']); ?>
+                <?php endif; ?>
+
                 <?php
                 if (isset($view)) {
                     require_once PATH_VIEW . $view . '.php';
